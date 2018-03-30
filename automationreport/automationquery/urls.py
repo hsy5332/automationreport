f9a844e181e15b10c651c526bb6c90e0a67642aa
steel <allenyao224@qq.com>
2018-03-30 18:25:44 +0800

    增加URL配置，view增加方法头

--- automationreport/automationquery/urls.py
+++ automationreport/automationquery/urls.py
@@ -3,7 +3,13 @@ from automationquery import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    url(r'^index', views.index),
-] + static(settings.STATIC_URL,docment_root = settings.STATIC_ROOT)
+                  url(r'^index', views.index),
+                  url(r'^login', views.login),
+                  url(r'^cpu', views.cpu),
+                  url(r'^launchapp', views.launchapp),
+                  url(r'^menapp', views.menapp),
+                  url(r'^interface', views.interface),
+                  url(r'^functionapp', views.functionapp),
+                  url(r'^functionweb', views.functionweb),
+              ] + static(settings.STATIC_URL, docment_root=settings.STATIC_ROOT)
--- automationreport/automationquery/views.py
+++ automationreport/automationquery/views.py
@@ -1,6 +1,54 @@
+import time
 from django.shortcuts import render
 from automationquery import models
 from django.http import HttpResponse
 
+
 def index(request):
+    return render(request, 'index.html')
+
+
+# token计算规则
+def token(token):
+    try:
+        if int(token) + 86400 > int(time.time()):  # 86400是一天的时间戳
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+# 登录接口
+def login(request):
+    return render(request, 'index.html')
+
+
+# cpu占有率
+def cpu(request):
+    return render(request, 'index.html')
+
+
+# 启动App时间查询
+def launchapp(request):
+    return render(request, 'index.html')
+
+
+# 内存占有率查询
+def menapp(request):
+    return render(request, 'index.html')
+
+
+# 接口测试查询
+def interface(request):
+    return render(request, 'index.html')
+
+
+# App功能测试查询
+def functionapp(request):
+    return render(request, 'index.html')
+
+
+# Web功能测试查询
+def functionweb(request):
     return render(request, 'index.html')
