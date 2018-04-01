7eccdce4d710c4944c9766bff65766d076c6e1a0
鈊诺 <allenyao224@qq.com>
2018-04-01 12:28:03 +0800

    更新接口文档,增加查询cpu、启动时间获取数据接口

--- automationreport/automationquery/views.py
+++ automationreport/automationquery/views.py
@@ -1,5 +1,6 @@
 import time
 import json
+import datetime
 
 from django.shortcuts import render
 from automationquery import models
@@ -8,7 +9,7 @@ from django.http import HttpResponse
 
 def index(request):
     userinfo = models.automation_user.objects.get(id=1)
-    return render(request, 'index.html',{"TEST": userinfo.username})
+    return render(request, 'index.html', {"TEST": userinfo.username})
 
 
 # token计算规则
@@ -48,25 +49,122 @@ def login(request):
                     login_usererror = {"code": "101", "msg": "该用户账号异常，请联系管理员。", "data": {}}
                     return HttpResponse(json.dumps(login_usererror))
             except:
-                login_accounterror = {"code" : "102","msg" : "账号或密码错误", "data" : {}}
+                login_accounterror = {"code": "102", "msg": "账号或密码错误", "data": {}}
                 return HttpResponse(json.dumps(login_accounterror))
         else:
-            login_tokenerror = {"code" : "-11", "msg" : "token过期", "data" : {}}
+            login_tokenerror = {"code": "-11", "msg": "token过期", "data": {}}
             return HttpResponse(json.dumps(login_tokenerror))
     else:
-        login_error = {"code" : "-12", "msg" : "请求方式错误", "data" : {}}
+        login_error = {"code": "-12", "msg": "请求方式错误", "data": {}}
         return HttpResponse(json.dumps(login_error))
-
 
 
 # cpu占有率
 def cpu(request):
-    return render(request, 'index.html')
+    if request.POST:
+        if token(request.POST['token']):
+            try:
+                if request.POST['startdate'] and request.POST['enddate'] != "":
+                    startdate = request.POST['startdate']
+                    enddate = request.POST['enddate']
+                    querydatas = models.automation_launch_app.objects.filter(caseid=request.POST['caseid'])
+                    querydatalist = []
+                    for querydata in range(0, len(querydatas)):
+                        if querydatas[querydata].createdtime <= enddate and querydatas[
+                            querydata].createdtime >= startdate:
+                            cpu_querydata = {
+                                "id": querydatas[querydata].id,
+                                "caseid": querydatas[querydata].caseid,
+                                "cpuproportion": querydatas[querydata].cpuproportion,
+                                "starttime": querydatas[querydata].starttime,
+                                "endtime": querydatas[querydata].endtime,
+                                "monkeyscript": querydatas[querydata].monkeyscript,
+                                "functionscript": querydatas[querydata].functionscript,
+                                "runtime": querydatas[querydata].runtime,
+                                "eventid": querydatas[querydata].eventid,
+                                "createdtime": querydatas[querydata].createdtime
+                            }
+                            querydatalist.append(cpu_querydata)
+                    return HttpResponse(json.dumps({"code": "200", "msg": "succes", "data": querydatalist}))
+                else:
+                    querydatas = models.automation_launch_app.objects.filter(caseid=request.POST['caseid'])
+                    querydatalist = []
+                    for querydata in range(0, len(querydatas)):
+                        cpu_querydata = {
+                            "id": querydatas[querydata].id,
+                            "caseid": querydatas[querydata].caseid,
+                            "cpuproportion": querydatas[querydata].cpuproportion,
+                            "starttime": querydatas[querydata].starttime,
+                            "endtime": querydatas[querydata].endtime,
+                            "monkeyscript": querydatas[querydata].monkeyscript,
+                            "functionscript": querydatas[querydata].functionscript,
+                            "runtime": querydatas[querydata].runtime,
+                            "eventid": querydatas[querydata].eventid,
+                            "createdtime": querydatas[querydata].createdtime
+                        }
+                        querydatalist.append(cpu_querydata)
+                    return HttpResponse(json.dumps({"code": "200", "msg": "succes", "data": querydatalist}))
+            except:
+                return HttpResponse(json.dumps({"code": "-13", "msg": "查询数据出错，请检查参数。", "data": {}}))
+        else:
+            cpu_tokenerror = {"code": "-11", "msg": "token过期", "data": {}}
+            return HttpResponse(json.dumps(cpu_tokenerror))
+    else:
+        cpu_error = {"code": "-12", "msg": "请求方式错误", "data": {}}
+        return HttpResponse(json.dumps(cpu_error))
 
 
 # 启动App时间查询
 def launchapp(request):
-    return render(request, 'index.html')
+    if request.POST:
+        if token(request.POST['token']):
+            try:
+                if request.POST['startdate'] and request.POST['enddate'] != "":
+                    startdate = request.POST['startdate']
+                    enddate = request.POST['enddate']
+                    launchappdatas = models.automation_launch_app.objects.filter(caseid=request.POST['caseid'])
+                    launchappdatalist = []
+                    for launchappdata in range(0, len(launchappdatas)):
+                        if launchappdatas[launchappdata].createdtime <= enddate and launchappdatas[
+                            launchappdata].createdtime >= startdate:
+                            print(launchappdatas[0].id)
+                            cpu_querydata = {
+                                "id": launchappdatas[launchappdata].id,
+                                "starttime": launchappdatas[launchappdata].starttime,
+                                "launchtime": launchappdatas[launchappdata].launchtime,
+                                "endtime": launchappdatas[launchappdata].endtime,
+                                "launchtype": launchappdatas[launchappdata].launchtype,
+                                "eventid": launchappdatas[launchappdata].eventid,
+                                "caseid": launchappdatas[launchappdata].caseid,
+                                "createdtime": launchappdatas[launchappdata].createdtime
+                            }
+                            launchappdatalist.append(cpu_querydata)
+                    return HttpResponse(json.dumps({"code": "200", "msg": "succes", "data": launchappdatalist}))
+                else:
+                    launchappdatas = models.automation_cpu_app.objects.filter(caseid=request.POST['caseid'])
+                    launchappdatalist = []
+                    for launchappdata in range(0, len(launchappdatas)):
+                        cpu_querydata = {
+                            "id": launchappdatas[launchappdata].id,
+                            "starttime": launchappdatas[launchappdata].starttime,
+                            "launchtime": launchappdatas[launchappdata].launchtime,
+                            "endtime": launchappdatas[launchappdata].endtime,
+                            "launchtype": launchappdatas[launchappdata].launchtype,
+                            "launchtime": launchappdatas[launchappdata].launchtime,
+                            "eventid": launchappdatas[launchappdata].eventid,
+                            "caseid": launchappdatas[launchappdata].caseid,
+                            "createdtime": launchappdatas[launchappdata].createdtime
+                        }
+                        launchappdatalist.append(cpu_querydata)
+                    return HttpResponse(json.dumps({"code": "200", "msg": "succes", "data": launchappdatalist}))
+            except:
+                return HttpResponse(json.dumps({"code": "-13", "msg": "查询数据出错，请检查参数。", "data": {}}))
+        else:
+            launchapp = {"code": "-11", "msg": "token过期", "data": {}}
+            return HttpResponse(json.dumps(launchapp))
+    else:
+        launchapp_error = {"code": "-12", "msg": "请求方式错误", "data": {}}
+        return HttpResponse(json.dumps(launchapp_error))
 
 
 # 内存占有率查询
